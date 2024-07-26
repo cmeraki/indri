7df016731023430a92a022545d44634a539b6aa1
4g <apurvagup@gmail.com>
2024-07-26 15:51:16 +0530

    adding models to hf and adding download script

---
+++ models/download.py
@@ -0,0 +1,13 @@
+from huggingface_hub import snapshot_download
+from os import getenv
+
+token = getenv('HF_TOKEN')
+
+print("Token", token)
+
+if (not token) or token[:3]!='hf_':
+    print("Token missing or incorrect. Add token as env variable HF_TOKEN")
+
+else:
+    snapshot_download('cmeraki/audio', repo_type='model', local_dir='audio_models', token=token)
+    snapshot_download('cmeraki/chameleon_tokenizer', repo_type='model', local_dir='chameleon_tokenizer', token=token)
