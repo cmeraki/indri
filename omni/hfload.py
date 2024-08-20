372601e1fccc0ec61b43eaf336ee0e396d87935a
romit <11757603+romitjain@users.noreply.github.com>
2024-08-20 20:30:56 +0530

    Added notebook, script in next commit

--- omni/hfload.py
+++ omni/hfload.py
@@ -1,51 +1,55 @@
 import torch
 from transformers import GPT2LMHeadModel, GPT2Config
 
-from common import device
-from common import Config as cfg
+from tts.gpt2_model import get_model
 
 # inverse of : https://github.com/karpathy/nanoGPT/blob/9755682b981a45507f6eb9b11eadef8cb83cebd5/model.py#L207
 # converts a saved nanogpt model back to hf format so it can be loaded in vllm
-# hf sampling is different so results are also different
+
+def load_model(path):
+    print(path)
+    model = get_model(
+        vocab_size=53376,
+        device='cpu',
+        compile=False,
+        path=path
+    )
+
+    model.eval()
+    return model
 
 def load_as_hf_model(path):
-    config = GPT2Config(vocab_size = cfg.VOCAB_SIZE,
-                            n_positions = 1024,
-                            n_embd = 768,
-                            n_layer = 12,
-                            n_head = 12,
-                            use_bias=False)
+    custom_gpt = load_model(path)
+    custom_gpt = custom_gpt.state_dict()
 
-    model = GPT2LMHeadModel(config)
-    sd_hf = model.state_dict()
-    k_hf = list(sd_hf.keys())
+    config = GPT2Config(
+        vocab_size=53376,
+        n_positions=1024,
+        n_embd=768,
+        n_layer=12,
+        n_head=12,
+        use_bias=False,
+        dropout=0,
+        attn_pdrop=0,
+        embd_pdrop=0,
+        resid_pdrop=0,
+        summary_first_dropout=0,
+        activation_function='gelu'
+    )
 
-    sd_ngpt = torch.load(path)['model']
-
+    clean_custom_gpt = {}
     unwanted_prefix = '_orig_mod.'
-    for k,v in list(sd_ngpt.items()):
+    for k, v in custom_gpt.items():
         if k.startswith(unwanted_prefix):
-            sd_ngpt[k[len(unwanted_prefix):]] = sd_ngpt.pop(k)
+            clean_custom_gpt[k[len(unwanted_prefix):]] = custom_gpt[k]
 
-    k_ngpt = list(sd_ngpt.keys())
+    transposed = [
+        'attn.c_attn.weight',
+        'attn.c_proj.weight',
+        'mlp.c_fc.weight',
+        'mlp.c_proj.weight'
+    ]
 
-    a = set(k_hf) - set(k_ngpt) 
-    a = [i for i in a if 'bias' not in i]
-
-    transposed = ['attn.c_attn.weight', 
-                  'attn.c_proj.weight', 
-                  'mlp.c_fc.weight', 
-                  'mlp.c_proj.weight']
-    
-    for k in k_ngpt:
+    for k, v in clean_custom_gpt.items():
         if any(k.endswith(w) for w in transposed):
-            assert sd_hf[k].shape[::-1] == sd_ngpt[k].shape
-            with torch.no_grad():
-                sd_hf[k].copy_(sd_ngpt[k].t())
-        else:
-            assert sd_hf[k].shape == sd_ngpt[k].shape
-            with torch.no_grad():
-                sd_hf[k].copy_(sd_ngpt[k])
-    
-    model.to(device)
-    return model
+            clean_custom_gpt[k] = clean_custom_gpt[k].t()
