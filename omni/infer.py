91983a64789bb72d9c99ea9e9e971e5ae0142dd0
4g <apurvagup@gmail.com>
2024-08-25 23:36:58 +0530

    adding omni app and silencing the always so eager tqdm in bark

--- omni/infer.py
+++ omni/infer.py
@@ -1,69 +1,20 @@
 import numpy as np
 import torch
-from pathlib import Path
-from huggingface_hub import snapshot_download
 
 from encodec.utils import save_audio
 
-from tts.gpt2_model import get_model, GPT
-from tts.train import DataLoader
 from common import DEVICE, ctx, TEXT, SEMANTIC
 from common import Config as cfg
-from datalib.tokenlib import get_tokenizer
-from common import cache_dir
 
 from common import Config as cfg
-from tts.utils import read_audio_file
 from tts.infer import AudioSemantic
-import simpleaudio as sa
+
+from tts.hfload import convert_to_hf
+    
 
 
-def playwav(wav):
-    wave_obj = sa.WaveObject.from_wave_file(wav)
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
-
-
-def extract_new_tokens(y, target):
-    start_idx = np.where(y == cfg.INFER_TOKEN[target])[0]
-    end_idx = np.where(y == cfg.STOP_TOKEN[target])[0]
-    if end_idx.any():
-        y = y[start_idx[0] + 1: end_idx[0]]
-    else:
-        y = y[start_idx[0] + 1:]
-
-    return y
-
-
-def generate_next(prompt, modality, model):
-    alltokens = np.hstack([prompt, [cfg.INFER_TOKEN[modality]]])
-
-    model.generation_config.eos_token_id = cfg.STOP_TOKEN[modality]
-    # model.generation_config.pad_token_id = cfg.STOP_TOKEN[modality]
-
-    input_tokens = (torch.tensor(alltokens,
-                                 dtype=torch.long,
-                                 device=DEVICE)[None, ...])
-
-    print(input_tokens)
-    with ctx:
-        target_tokens = model.generate(input_tokens,
-                                       max_length=1024,
-                                       temperature=0.7,
-                                       top_k=100,
-                                       do_sample=True)
-
-        target_tokens = target_tokens.detach().cpu().numpy()[0]
-        target_tokens = target_tokens[len(alltokens):]
-
-    end_idx = np.where(target_tokens == cfg.STOP_TOKEN[modality])[0][0]
-    target_tokens = target_tokens[0:end_idx]
-    print(f'generated_{modality}', target_tokens)
-    # target_tokens = extract_new_tokens(target_tokens, target=modality)
-
-    target_tokens = target_tokens - cfg.OFFSET[modality]
-
-    return target_tokens
+tokenizer = AudioSemantic(size='125m')
+omni_model = convert_to_hf(path=f'/home/apurva/Downloads/omni.pt', device=DEVICE)
 
 
 def decorate(tokens, type):
@@ -73,38 +24,38 @@ def decorate(tokens, type):
                         cfg.STOP_TOKEN[type]])
     return tokens
 
-
 def converse():
-    from tts.hfload import convert_to_hf
-    omni_model = convert_to_hf(path=f'/home/meraki/Downloads/omni.pt', device=DEVICE)
-    print(omni_model)
-
-    tokenizer = AudioSemantic(size='125m')
-
     human_text = 'once upon a time there was a girl named mary'
 
     human_text_tokens = tokenizer.text_tokenizer.encode(human_text)
     human_text_tokens = np.asarray(human_text_tokens)
     human_text_tokens = decorate(human_text_tokens, type=TEXT)
-    t = tokenizer.text_tokenizer.decode(human_text_tokens)
-    # print("DC", t)
 
-    for i in range(10):
-        next_text_tokens = generate_next(human_text_tokens, TEXT, model=omni_model)
-        next_semantic_tokens = generate_next(human_text_tokens, SEMANTIC, model=omni_model)
+    alltokens = np.hstack([human_text_tokens, [cfg.INFER_TOKEN[SEMANTIC]]])
 
-        next_text = tokenizer.text_tokenizer.decode(next_text_tokens)
+    omni_model.generation_config.eos_token_id = cfg.STOP_TOKEN[SEMANTIC]
 
-        print("text", next_text)
-        print("semantic", list(next_semantic_tokens[:256]), next_semantic_tokens.shape)
+    input_tokens = (torch.tensor(alltokens,
+                                 dtype=torch.long,
+                                 device=DEVICE)[None, ...])
+    with ctx:
+        target_tokens = omni_model.generate(input_tokens,
+                                       max_length=1024,
+                                       temperature=0.7,
+                                       top_k=100,
+                                       do_sample=True)
 
-        # uncomment to speak text via tts
-        # next_semantic_tokens = tokenizer.text_to_semantic(next_text)
+        target_tokens = target_tokens.detach().cpu().numpy()[0]
+        target_tokens = target_tokens[len(alltokens):]
 
-        next_audio = tokenizer.semantic_to_audio(next_semantic_tokens)
-        tmp_audio_file = f'omni_{i}.wav'
-        save_audio(next_audio[0], tmp_audio_file, sample_rate=24000)
-        playwav(tmp_audio_file)
+    end_idx = np.where(target_tokens == cfg.STOP_TOKEN[SEMANTIC])[0][0]
+    target_tokens = target_tokens[0:end_idx]
+    target_tokens = target_tokens - cfg.OFFSET[SEMANTIC]
+
+    next_audio = tokenizer.semantic_to_audio(target_tokens)
+    tmp_audio_file = f'omni.wav'
+    save_audio(next_audio[0], tmp_audio_file, sample_rate=24000)
+    # playwav(tmp_audio_file)
 
 
 if __name__ == "__main__":
