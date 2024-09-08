02487fc60481bc2881fcdab765fea2f1a5ec50ce
romit <11757603+romitjain@users.noreply.github.com>
2024-09-08 23:18:51 +0530

    Updated app to support new task definition

--- omni/app.py
+++ omni/app.py
@@ -1,109 +1,193 @@
 import torch
 import gradio as gr
 import numpy as np
+from pathlib import Path
 from huggingface_hub import snapshot_download
 from encodec.utils import save_audio
 
+from configs.commons import Config as cfg
+from configs.commons import DEVICE, CACHE_DIR, CTX
+from configs.constants import *
+
 from omni.hfload import convert_to_hf
 from datalib.tokenlib import get_tokenizer
-from common import Config as cfg, ctx, cache_dir, DEVICE, TEXT, SEMANTIC, ACOUSTIC
+from omni.train_omni_instruct import DataLoader
 
-local_dir = f'{cache_dir}/models/omni_774m_tinystories'
-snapshot_download(f'cmeraki/omni_774m_tinystories', local_dir=local_dir)
-omni_model = convert_to_hf(path=f'{local_dir}/omni.pt', device=DEVICE)
+local_dir = f'{CACHE_DIR}/models/omni_774m_tinystories'
+omni_model = convert_to_hf(
+    path=f'/home/.cache/indri/models/omni_tasks_large/gpt_2900.pt',
+    device=DEVICE
+)
+semantic_acoustic_model = convert_to_hf(
+    path=f'/home/.cache/indri/romit/models/semantic_acoustic_tasks_small/gpt_4500.pt',
+    device=DEVICE
+)
 
-text_tokenizer = get_tokenizer(TEXT, device='cpu')
 acoustic_tokenizer = get_tokenizer(ACOUSTIC, device=DEVICE)
 
-local_dir = f'{cache_dir}/models/tts_xl_30k_long_125m_en/semantic_acoustic/'
-snapshot_download(f'cmeraki/tts_xl_30k_long_125m_en/semantic_acoustic/', local_dir=local_dir)
-semantic_acoustic_model = convert_to_hf(path=f'{local_dir}/gpt_last.pt', device=DEVICE)
+# snapshot_download(f'cmeraki/tts_xl_30k_long_125m_en/semantic_acoustic/', local_dir=local_dir)
 
-def decorate(tokens, type):
-    tokens = tokens + cfg.OFFSET[type]
-    tokens = np.hstack([cfg.INFER_TOKEN[type],
-                        tokens,
-                        cfg.STOP_TOKEN[type]])
-    return tokens
+# Common tokenization
+dl = DataLoader(
+    interleaved_dirs=[],
+    datasets_dirs=[],
+    speaker_files=[Path('../allowed_speakers.jsonl').resolve()]
+)
 
-def extract_new_tokens(y, target):
-    start_idx = np.where(y == cfg.INFER_TOKEN[target])[0]
-    end_idx = np.where(y == cfg.STOP_TOKEN[target])[0]
-    if end_idx.any():
-        y = y[start_idx[0] + 1: end_idx[0]]
-    else:
-        y = y[start_idx[0] + 1:]
+text_tokenizer = dl.text_tokenizer
+acoustic_modality_token = text_tokenizer.encode(cfg.MODALITY_TOKENS[ACOUSTIC])
 
-    return y
+omni_model.generation_config.eos_token_id = dl.stop_token
+semantic_acoustic_model.generation_config.eos_token_id = dl.stop_token
 
-def converse(text):
-    human_text = text
+SPEAKERS = {
+    "ASMR": "[spkr_asmr]",
+    "Cartoon": "[spkr_cartoon]",
+    "Jenny": "[spkr_jenny_jenny]",
+    "Random": "[spkr_unk]"
+}
 
-    human_text_tokens = text_tokenizer.encode(human_text)
-    human_text_tokens = np.asarray(human_text_tokens)
-    human_text_tokens = decorate(human_text_tokens, type=TEXT)
 
-    alltokens = np.hstack([human_text_tokens, [cfg.INFER_TOKEN[SEMANTIC]]])
+def create_omni_tokens(task, incoming_tokens, incoming_modality, speaker_id = '[spkr_unk]'):
+    if task == TTS:
+        input_tokens = np.hstack([
+            dl.text_modality_token,
+            incoming_tokens,
+            dl.convert_tokens,
+            dl.semantic_modality_token,
+            text_tokenizer.encode(speaker_id)
+        ])
 
-    omni_model.generation_config.eos_token_id = cfg.STOP_TOKEN[SEMANTIC]
+    elif task == ASR:
+        input_tokens = np.hstack([
+            dl.semantic_modality_token,
+            text_tokenizer.encode(speaker_id),
+            incoming_tokens,
+            dl.convert_token,
+            dl.text_modality_token
+        ])
 
-    input_tokens = (torch.tensor(alltokens, dtype=torch.long, device=DEVICE)[None, ...])
+    elif task == CONTINUE and incoming_modality == TEXT:
+        input_tokens = np.hstack([
+            dl.text_modality_token,
+            incoming_tokens
+        ])
+
+    elif task == CONTINUE and incoming_modality == AUDIO:
+        input_tokens = np.hstack([
+            dl.semantic_modality_token,
+            text_tokenizer.encode(speaker_id),
+            incoming_tokens
+        ])
+
+    print(f'Input tokens: {text_tokenizer.decode(input_tokens)}, shape: {input_tokens.shape}')
+    return input_tokens
+
+
+def create_semaco_tokens(incoming_tokens, speaker_id = '[spkr_unk]'):
+    input_tokens = np.hstack([
+        dl.semantic_modality_token,
+        text_tokenizer.encode(speaker_id),
+        incoming_tokens,
+        dl.convert_tokens,
+        acoustic_modality_token
+    ])
+
+    print(f'Input tokens: {text_tokenizer.decode(input_tokens)}, shape: {input_tokens.shape}')
+    return input_tokens
+
+
+def converse(task, text_input, audio_input, speaker):
+
+    if text_input:
+        assert audio_input is None, "Cannot provide both text and audio inputs"
+        assert task in [TTS, CONTINUE], "Task must be TTS or Continue for text input"
+
+        input_tokens = create_omni_tokens(task, text_input, TEXT, speaker)
+
+    elif audio_input:
+        assert text_input is None, "Cannot provide both audio and text inputs"
+        assert task in [ASR, CONTINUE], "Task must be ASR or Continue for audio input"
+
+        input_tokens = create_omni_tokens(task, audio_input, AUDIO, speaker)
+
+    input_tokens = (torch.tensor(input_tokens, dtype=torch.long, device=DEVICE)[None, ...])
 
     # Text -> Semantic/Text
-    with ctx:
-        semantic_tokens = omni_model.generate(
+    with CTX:
+        omni_output = omni_model.generate(
             input_tokens,
             max_length=1024,
-            temperature=0.7,
+            temperature=0.8,
             top_k=100,
             do_sample=True
         )
 
-        semantic_tokens = semantic_tokens.detach().cpu().numpy()[0]
-        semantic_tokens = semantic_tokens[len(alltokens):]
+        omni_output = omni_output.detach().cpu().numpy()[0]
+        omni_output = omni_output[len(input_tokens):]
 
-    max_source_semantic_tokens = cfg.MAX_SOURCE_TOKENS[SEMANTIC]
-    end_idx = np.where(semantic_tokens == cfg.STOP_TOKEN[SEMANTIC])[0][0]
-    semantic_tokens = semantic_tokens[0:end_idx]
-    semantic_tokens = semantic_tokens[:max_source_semantic_tokens]
+    end_idx = np.where(omni_output == dl.stop_token)[0][0]
+    omni_output = omni_output[:end_idx]
+    print(f'Omni output: {text_tokenizer.decode(omni_output)}, shape: {omni_output.shape}')
 
-    semantic_tokens = np.hstack([semantic_tokens, cfg.INFER_TOKEN[ACOUSTIC]])
-    semantic_tokens = (torch.tensor(semantic_tokens, dtype=torch.long,device=DEVICE)[None, ...])
+    if task in [CONTINUE, ASR] and text_input:
+        return text_tokenizer.decode(omni_output), None, None
+
+    semantic_tokens = create_semaco_tokens(omni_output)
+    semantic_tokens = (torch.tensor(semantic_tokens, dtype=torch.long, device=DEVICE)[None, ...])
 
     # Semantic -> Acoustic
-    with ctx:
+    with CTX:
         acoustic_tokens = semantic_acoustic_model.generate(
             semantic_tokens,
-            max_length=cfg.BLOCK_SIZE[SEMANTIC],
-            temperature=0.95,
+            max_length=3072,
+            temperature=0.8,
             top_k=100,
             do_sample=True
         )
         acoustic_tokens = acoustic_tokens.detach().cpu().numpy()[0]
+        acoustic_tokens = acoustic_tokens[len(semantic_tokens):]
 
-    acoustic_tokens = extract_new_tokens(acoustic_tokens, target=ACOUSTIC)
+    end_idx = np.where(acoustic_tokens == dl.stop_token)[0][0]
+    acoustic_tokens = acoustic_tokens[0:end_idx]
     acoustic_tokens = acoustic_tokens - cfg.OFFSET[ACOUSTIC]
+
     if len(acoustic_tokens) % 2 == 1:
         acoustic_tokens = acoustic_tokens[:-1]
+
+    print(f'Acoustic output: {acoustic_tokens}, shape: {acoustic_tokens.shape}')
 
     # Acoustic -> Audio
     wav = acoustic_tokenizer.decode(torch.tensor(acoustic_tokens))
     wav = wav[0].cpu()
+
     tmp_audio_file = f'omni.wav'
     save_audio(wav, tmp_audio_file, sample_rate=24000)
 
-    return 24_000, wav[0].numpy()
+    return None, 24_000, wav[0].numpy()
 
+with gr.Blocks() as demo:
+    gr.Markdown("## Omni")
+    
+    with gr.Row():
+        task = gr.Dropdown(["ASR", "TTS", "Continue Text", "Continue Audio"], label="Select Task")
+        speaker = gr.Dropdown(list(SPEAKERS.keys()), label="Select Speaker")
 
-demo = gr.Interface(
-    fn=converse,
-    inputs=[
-        gr.Textbox(label="Text to continue"),
-    ],
-    outputs=gr.Audio(label="Generated Audio", autoplay=True),
-    title="Text-Audio Multimodal",
-    description="Enter text to continue audio"
-)
+    with gr.Row():
+        text_input = gr.Textbox(label="Text Input")
+        audio_input = gr.Audio(sources=["microphone", "upload"], label="Audio Input", type="numpy")
+    
+    with gr.Row():
+        text_output = gr.Textbox(label="Text Output")
+        audio_output = gr.Audio(label="Audio Output")
+
+    submit_button = gr.Button("Submit")
+
+    submit_button.click(
+        fn=converse,
+        inputs=[task, text_input, audio_input, SPEAKERS[speaker]],
+        outputs=[text_output, audio_output]
+    )
 
 if __name__ == "__main__":
     demo.launch(server_name="0.0.0.0", server_port=6006)
