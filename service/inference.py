b8f18062c3634843b941e44a8b7886b3708fe680
romitjain <romit@merakilabs.com>
2024-10-09 18:05:06 +0530

    vLLM support with mimi model

---
+++ service/inference.py
@@ -0,0 +1,40 @@
+import base64
+import numpy as np
+from typing import List
+from pydantic import BaseModel
+from pathlib import Path
+from fastapi import FastAPI, HTTPException
+
+
+from omni.logger import get_logger
+from service.tts import TTS
+
+logger = get_logger(__name__)
+
+app = FastAPI()
+
+global model
+model = TTS(Path('~/projects/romit/mimi_hf').expanduser(), device='cuda:1')
+
+class TTSRequest(BaseModel):
+    text: str
+
+# class TTSResponse(BaseModel):
+#     audio: np.ndarray
+
+@app.post("/tts")#, response_model=TTSResponse)
+def text_to_speech(requests: TTSRequest):
+    logger.info(f'Received text: {requests.text}')
+
+    results = model.generate(requests.text)
+    encoded = base64.b64encode(results.tobytes()).decode('utf-8')
+
+    return {
+        "array": encoded,
+        "dtype": str(results.dtype),
+        "shape": results.shape
+    }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- service/tts.py
+++ service/tts.py
@@ -1,23 +1,32 @@
+import os
+import torch
 import numpy as np
 from transformers import MimiModel
 from vllm import LLM, SamplingParams
+from typing import List
 
-from omni.config import cfg
+from commons import CTX, TEXT, MIMI, CONVERT
+from commons import Config as cfg
 from omni.logger import get_logger
 from omni.train import get_text_tokenizer
+import service.utils as utils
 
 logger = get_logger(__name__)
 
 class TTS:
-    def __init__(self, model_path):
-        self.audio_tokenizer = MimiModel.from_pretrained("kyutai/mimi")
+    def __init__(self, model_path, device):
+        self.device = device
+        self.audio_tokenizer = MimiModel.from_pretrained("kyutai/mimi").to(device=self.device)
+        os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
         self.model = LLM(
             model=model_path,
             skip_tokenizer_init=True,
-            gpu_memory_utilization=0.6,
+            gpu_memory_utilization=0.4,
             dtype='float32'
         )
+
+        self.audio_tokenizer.eval()
 
         self.text_tokenizer = get_text_tokenizer()
 
@@ -32,38 +41,57 @@ class TTS:
             stop_token_ids=self.stop_token
         )
 
+    def preprocess_text(self, text: str) -> List[str]:
+        text = utils.normalize_text(text)
+        text = utils.split_and_join_sentences(text)
+
+        return text
+
     def prepare_tokens(self, incoming_text, speaker) -> List[int]:
-        incoming_tokens = text_tokenizer.encode(incoming_text)
+        incoming_tokens = self.text_tokenizer.encode(incoming_text)
 
         input_tokens = np.hstack([
-            dl.text_modality_token,
+            self.text_modality_token,
             incoming_tokens,
-            dl.convert_token,
-            dl.semantic_modality_token,
-            text_tokenizer.encode(speaker)
+            self.convert_token,
+            self.acoustic_modality_token,
+            self.text_tokenizer.encode(speaker)
         ])
 
         return input_tokens.tolist()
 
-    def generate_batch(self, batch_text: List[str], speaker='[spkr_hifi_tts_9017]'):
+    def generate(
+        self,
+        text: List[str],
+        speaker='[spkr_hifi_tts_9017]',
+        batch_size=32
+    ):
+        batch_text = self.preprocess_text(text)
         logger.info(f'Texts after preprocessing: {batch_text}')
-        input_tokens = [prepare_tokens(text, speaker) for text in batch_text]
-        logger.info(f'Batch after preprocessing: {batch_text}')
+        input_tokens = [self.prepare_tokens(text, speaker) for text in batch_text]
+        logger.info(f'Input tokens: {input_tokens} and batch size: {batch_size}')
 
-        outputs = self.model.generate(
-            input_tokens,
-            self.sampling_params
+        out = self.model.generate(
+            prompt_token_ids=input_tokens,
+            sampling_params=self.sampling_params
         )
 
         mimi_tokens = []
-
-        for o in outputs:
+        for o in out:
             o = o.outputs[0].token_ids
-            logger.info(f'Mimi tokens: {self.text_tokenizer.decode(o)}')
-
+            o = np.array(o)
+            print(o)
             o = o - cfg.OFFSET[MIMI]
             mimi_tokens.extend(o)
 
-        mimi_tokens = deserialize_tokens(mimi_tokens)
+        mimi_tokens = np.hstack(mimi_tokens)
 
-        return mimi_tokens
+        logger.info(f'Mimi tokens: {mimi_tokens}')
+        mimi_tokens = utils.deserialize_tokens(mimi_tokens)
+
+        with torch.no_grad():
+            audio = self.audio_tokenizer.decode(
+                torch.tensor(np.expand_dims(mimi_tokens, axis=0), device=self.device)
+            ).audio_values
+
+        return audio.cpu().numpy()
