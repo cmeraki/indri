79d98d73655d6324283bf65d50cec71725893088
compute <compute@merakilabs.com>
2024-10-09 13:37:29 +0530

    batching whisper

--- datalib/create_dataset_from_audio.py
+++ datalib/create_dataset_from_audio.py
@@ -9,21 +9,13 @@ import numpy as np
 import torch
 import os
 import torchaudio
-
+from tqdm import tqdm
 from silero_vad import load_silero_vad, read_audio, get_speech_timestamps, save_audio
-silero = load_silero_vad()
 
-def convert_audio(wav: torch.Tensor, sr: int, target_sr: int, target_channels: int):
-    assert wav.shape[0] in [1, 2], "Audio must be mono or stereo."
-    if target_channels == 1:
-        wav = wav.mean(0, keepdim=True)
-    elif target_channels == 2:
-        *shape, _, length = wav.shape
-        wav = wav.expand(*shape, target_channels, length)
-    elif wav.shape[0] == 1:
-        wav = wav.expand(target_channels, -1)
-    wav = torchaudio.transforms.Resample(sr, target_sr)(wav)
-    return wav
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from datasets import load_dataset
+
 
 def find_audio_files(folder):
     audio_extensions = ('.mp3', '.flac', '.wav', '.ogg')
@@ -35,56 +27,90 @@ def find_audio_files(folder):
 
     return audio_files
 
-@torch.inference_mode()
-def get_transcript(audio_path):
-    print("WORKING ON", audio_path)
-    wav = read_audio(audio_path)
-    speech_timestamps = get_speech_timestamps(wav, silero)
-    from tqdm import tqdm
-    text = ''
-    data = []
-    for timestamp in speech_timestamps:
-        subwav = wav[timestamp['start']:timestamp['end']]
-        transcript = whisper_model.transcribe(audio=subwav, initial_prompt=text) 
-        text += transcript['text']
-        transcript['audio'] = subwav
-        yield transcript
-    
+
+class Transcriber:
+    def __init__(self, device):
+        self.silero = load_silero_vad()
+
+        model_id = "openai/whisper-large-v3-turbo"
+
+        model = AutoModelForSpeechSeq2Seq.from_pretrained(
+            model_id, low_cpu_mem_usage=True, use_safetensors=True
+        )
+        model.to(device)
+
+        processor = AutoProcessor.from_pretrained(model_id)
+
+        self.pipe = pipeline(
+            "automatic-speech-recognition",
+            model=model,
+            tokenizer=processor.tokenizer,
+            feature_extractor=processor.feature_extractor,
+            device=device,
+        )
+
+    def detect_language(self, audio):
+        mel = self.whisper_model.log_mel_spectrogram(audio).to(self.whisper_model.device)
+        _, probs = self.whisper_model.detect_language(mel)
+        print(_, probs)
+        return probs
+
+    @torch.inference_mode()
+    def get_transcript(self, audio_path, verbose=True, batch_size=4):
+        langprobs = self.detect_language(audio_path)
+        print("Language probs", langprobs)
+
+        wav = read_audio(audio_path)
+        speech_timestamps = get_speech_timestamps(wav, self.silero)
+        
+        prev_text = ''
+        audio_batch = []
+
+        for timestamp in speech_timestamps:
+            subwav = wav[timestamp['start']:timestamp['end']]
+            audio_batch.append(subwav)
+            if len(audio_batch) >= batch_size:
+                transcript = self.pipe(audio=audio_batch,
+                                       batch_size=batch_size)
+                print(transcript)
+
+                if verbose:
+                    print(timestamp, transcript['text'])
+                
+                text = transcript['text']
+            transcript['audio'] = subwav
+            yield transcript
+
     
 def process(dsname, input_audio_dir, speaker_name):
     dataset = Dataset(repo_id=dsname)
     audio_files = find_audio_files(input_audio_dir)
 
     print("num audio files", len(audio_files))
-    # print(audio_files[51])
-    # audio_files = audio_files[51:52]
+    transcriber = Transcriber()
 
     for file_idx, audio_file in tqdm(enumerate(audio_files), 'processing file ..'):
-        transcript = get_transcript(audio_file)
-
-        # transcript_path = Path(audio_file).with_suffix('.json')
-        # transcript_json = json.dumps(transcript, ensure_ascii=False)
-
-        # with open(transcript_path, 'w') as transcript_writer: 
-        #     transcript_writer.write(transcript_json)
+        print("WORKING ON", audio_path)
+        try:
+            transcript = transcriber.get_transcript(audio_file)
+            for chunk_idx, elem in tqdm(enumerate(transcript), desc='processing chunks..'):
+                id = f'{file_idx}_chunk_{chunk_idx}'
+                sample = Dataset.create_sample(id=id)
+                
+                sample.raw_text = elem['text']
+                sample.speaker_id = speaker_name
+                sample.audio_array = elem['audio'].numpy()
+                
+                sample.sampling_rate = 16000
+                sample.duration = len(sample.audio_array)/sample.sampling_rate
+                
+                audio_path = dataset.get_absolute_path(sample.audio_path)
+                save_audio(audio_path, elem['audio'], sampling_rate=16000)
+                # dataset.add_audio(sample)
+                dataset.add_metadata(sample)
         
-        
-        for chunk_idx, elem in tqdm(enumerate(transcript), desc='processing chunks..'):
-            id = f'{file_idx}_chunk_{chunk_idx}'
-            sample = Dataset.create_sample(id=id)
-            
-            sample.raw_text = elem['text']
-            sample.speaker_id = speaker_name
-            sample.audio_array = elem['audio'].numpy()
-            
-            sample.sampling_rate = 16000
-            sample.duration = len(sample.audio_array)/sample.sampling_rate
-            
-            audio_path = dataset.get_absolute_path(sample.audio_path)
-            save_audio(audio_path, elem['audio'], sampling_rate=16000)
-            # dataset.add_audio(sample)
-            dataset.add_metadata(sample)
-            
+        except:
+            print("FAILED", audio_path)        
 
 if __name__ == '__main__':
     import argparse
@@ -97,7 +123,6 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
 
-    whisper_model = whisper.load_model("turbo", device=args.device)
-    whisper_model.eval()
+
     
     process(args.dsname, args.audio, args.speaker)
