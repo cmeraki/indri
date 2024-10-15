b1ebd372ec49452841d4d93b3d42456d897f7829
4g <apurvagup@gmail.com>
2024-10-15 10:54:12 +0530

    listing hf files and test filter

--- datalib/webds_pipelines.py
+++ datalib/webds_pipelines.py
@@ -4,24 +4,29 @@ import webdataset as wds
 from pathlib import Path
 import os
 import torchaudio
+from huggingface_hub import list_repo_files
 
-def break_audio_chunks(input_ds, output_dir, output_ds):
+HF_TOKEN = os.environ['HF_TOKEN']
+
+def get_all_tars_in_hf_repo(repo_id, hf_token=HF_TOKEN):
+    files = list_repo_files(repo_id=repo_id, token=hf_token, repo_type='dataset')
+    return files
+
+
+def break_audio_chunks(hf_files, output_dir, output_ds):
     hf_token = os.environ['HF_TOKEN']
-    url = "https://huggingface.co/datasets/cmeraki/youtube_webdataset/resolve/main/en__storiesofmahabharata__{{000000..000000}}.tar"
-    url = f"pipe:curl -s -L {url} -H 'Authorization:Bearer {hf_token}'"
-
+    urls = []
+    for hf_file in hf_files:
+        url = f"https://huggingface.co/datasets/cmeraki/youtube_webdataset/resolve/main/{hf_file}"
+        url = f"pipe:curl -s -L {url} -H 'Authorization:Bearer {hf_token}'"
+        urls.append(url)
+    
     cache_dir = Path('~/.cache/wds/tmp/').expanduser()
     os.makedirs(cache_dir, exist_ok=True)
 
-    def get_sample(item):
-        txt = item['json']['raw_text']
-        audio = item['wav']
-
-        return txt, audio
-    
     transcriber = Transcriber(device='cuda:0')
 
-    dataset = wds.WebDataset(url, shardshuffle=None, cache_dir=cache_dir).decode()
+    dataset = wds.WebDataset(urls, shardshuffle=None, cache_dir=cache_dir).decode()
     
     dataloader = DataLoader(
         dataset=dataset,
@@ -36,4 +41,10 @@ def break_audio_chunks(input_ds, output_dir, output_ds):
         for chunk in transcriber.get_transcript(wav=wav[0]):
             print(chunk)
 
-break_audio_chunks(1,1,1)
+def whisper_on_hindi():
+    files = get_all_tars_in_hf_repo('cmeraki/youtube_webdataset')
+    en_files = list(filter(lambda x:x[:2] == 'en', files))
+    print(en_files)
+    break_audio_chunks(en_files,1,1)
+
+whisper_on_hindi()
