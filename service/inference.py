a62a53da71cdb9356f62baa76d300c422dc92444
romitjain <11757603+romitjain@users.noreply.github.com>
2024-10-29 05:53:46 +0000

    Added request id in inference

--- service/inference.py
+++ service/inference.py
@@ -3,6 +3,7 @@ sys.path.append('omni/')
 
 import time
 import base64
+import uuid
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -11,9 +12,6 @@ from .models import TTSRequest, TTSResponse, TTSSpeakersResponse, Speakers, spea
 from .logger import get_logger
 
 logger = get_logger(__name__)
-
-# DONE: Propogate speaker to tts
-# DONE: Exception handling
 
 app = FastAPI()
 
@@ -40,21 +38,23 @@ model = TTS(
 
 @app.post("/tts", response_model=TTSResponse)
 def text_to_speech(requests: TTSRequest):
+    request_id = str(uuid.uuid4())
+
     start_time = time.time()
-    logger.info(f'Received text: {requests.text} with speaker: {requests.speaker}')
+    logger.info(f'Received text: {requests.text} with speaker: {requests.speaker}', extra={'request_id': request_id})
 
     try:
         results = model.generate(requests.text, speaker_mapping(requests.speaker))
         audio = results['audio']
         metrics = results['metrics']
     except Exception as e:
-        logger.critical(f'Error in model generation: {e}')
+        logger.critical(f'Error in model generation: {e}', extra={'request_id': request_id})
         raise HTTPException(status_code=500, detail=str(e))
 
     end_time = time.time()
     metrics.end_to_end_time = end_time - start_time
 
-    logger.info(f'Metrics: {metrics}')
+    logger.info(f'Metrics: {metrics}', extra={'request_id': request_id})
 
     encoded = base64.b64encode(audio.tobytes()).decode('utf-8')
     return {
